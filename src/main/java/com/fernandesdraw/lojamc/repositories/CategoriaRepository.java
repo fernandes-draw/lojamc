b806f7604f8350a60d175fb71356acbefcc00c38
Everson Fernandes <fernandes_draw@hotmail.com>
2020-11-02 11:45:53 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/fernandesdraw/lojamc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.fernandesdraw.lojamc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fernandesdraw.lojamc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+	
+}
