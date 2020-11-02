4e502248cc6c354a9c5000a154d3557f25a5174d
Everson Fernandes <fernandes_draw@hotmail.com>
2020-11-02 09:24:28 -0300

    Projeto Criado

---
+++ src/test/java/com/fernandesdraw/lojamc/LojamcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandesdraw.lojamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojamcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
